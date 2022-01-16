a2f68ff353fc6ae04170f0a8ffec807c210792a9
Alex_K <79103208963@ya.ru>
2022-01-16 21:51:18 +0300

    Lesson 5. ORM filters

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,5 @@
-# import datetime
-
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
 from .models import Contact, Product, ProductCategory
@@ -9,15 +7,33 @@ from .models import Contact, Product, ProductCategory
 
 def main(request):
     title = "главная"
-    products = Product.objects.all()
-    # content = {"title": title, "products": products}
-    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
+
+    products = Product.objects.all()[:4]
+
+    content = {"title": title, "products": products,
+               "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by("price")
+            category = {"name": "все"}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(
+                category__pk=pk).order_by("price")
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+        }
+        return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
     content = {
         "title": title,
@@ -34,5 +50,6 @@ def contact(request):
     title = "о нас"
     visit_date = timezone.now()
     locations = Contact.objects.all()
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    content = {"title": title, "visit_date": visit_date,
+               "locations": locations}
     return render(request, "mainapp/contact.html", content)
