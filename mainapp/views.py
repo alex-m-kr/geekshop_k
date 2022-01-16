9c240e8e56bdaf47dfdb7db08c8362acc4cd4d20
Alex_K <79103208963@ya.ru>
2022-01-16 22:45:19 +0300

    Lesson 5. Code style fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,8 +12,7 @@ def main(request):
 
     products = Product.objects.all()[:4]
 
-    content = {"title": title, "products": products,
-               "media_url": settings.MEDIA_URL}
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
@@ -34,8 +33,7 @@ def products(request, pk=None):
             category = {"name": "все"}
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(
-                category__pk=pk).order_by("price")
+            products = Product.objects.filter(category__pk=pk).order_by("price")
         content = {
             "title": title,
             "links_menu": links_menu,
@@ -63,6 +61,5 @@ def contact(request):
     title = "о нас"
     visit_date = timezone.now()
     locations = Contact.objects.all()
-    content = {"title": title, "visit_date": visit_date,
-               "locations": locations}
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
